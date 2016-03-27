71cc0a5dac725119c62ea119a6f44c28cc724529
Evgerher <evgerher1l@gmail.com>
2016-03-27 20:17:12 +0600

    Test for a = 0, b !=0 and c!=0

--- src/root.c
+++ src/root.c
@@ -5,17 +5,22 @@ float solve(float a, float b, float c, float* x1, float* x2, int* roots_num)
 {
 	float d;
 	int roots = 0;
+	if (a > 0) {
+		d = b * b - 4 * a * c;
+		
+		if (d > 0)
+			roots = 2;
+		else if (d == 0) 
+			roots = 1;
 
-	d = b * b - 4 * a * c;
+		*x1 = (-b + sqrt(d)) / (2 * a);
+		*x2 = (-b - sqrt(d)) / (2 * a);
+		*roots_num = roots;
+
+		return d;
+	}
 	
-	if (d > 0)
-		roots = 2;
-	else if (d == 0) 
-		roots = 1;
-
-	*x1 = (-b + sqrt(d)) / (2 * a);
-	*x2 = (-b - sqrt(d)) / (2 * a);
 	*roots_num = roots;
 
-	return d;
+	return 0;
 }
