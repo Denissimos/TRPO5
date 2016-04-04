0131e5cb7883c4eb244e2611759476c96da3d35c
Evgerher <evgerher1l@gmail.com>
2016-04-05 01:10:46 +0600

    second test

--- test/root_test.c
+++ test/root_test.c
@@ -22,3 +22,24 @@ CTEST(quadratic_equation_suite, discriminant_test) {
     ASSERT_DBL_NEAR(expected_status, status);
 }
 
+CTEST(quadratic_equation_suite, two_roots_test) {
+    // Given
+    const float a = 5.4;
+    const float b = 32;
+    const float c = 1;
+
+    // When
+
+    Roots answer;
+    int status;
+    answer = solve(a, b, c, &status);
+
+    // Then
+    const int expected_status = 2;
+    const Roots expected_answer = {-0.031416556251127374, -5.894509369674798, 1002.4};
+
+    ASSERT_DBL_NEAR(expected_answer.discriminant, answer.discriminant);
+    ASSERT_DBL_NEAR(expected_status, status);
+    ASSERT_DBL_NEAR(expected_answer.x1, answer.x1);
+    ASSERT_DBL_NEAR(expected_answer.x2, answer.x2);
+}
